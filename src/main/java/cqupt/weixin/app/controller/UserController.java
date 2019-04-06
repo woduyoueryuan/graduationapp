2a947acd90b4726c9b27881463d6d9bf44e64427
yangfei <980997635@qq.com>
2019-04-06 16:51:59 +0800

    忽略规则

---
+++ src/main/java/cqupt/weixin/app/controller/UserController.java
@@ -0,0 +1,12 @@
+package cqupt.weixin.app.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class UserController {
+    //用户登陆
+    public void userLogin(){
+        //进行用户登陆。
+
+    }
+}
